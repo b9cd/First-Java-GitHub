b786e026d5a7570645021497c9cc021a4ec02552
ABDULRHMAN <abdsati6@icloud.com>
2023-07-29 02:16:35 +0300

    Adding age

--- src/com/voting/voting.java
+++ src/com/voting/voting.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 public class voting {
     public static void main(String[] args){
 
+        /*Asking for name*/
         Scanner nameIn = new Scanner(System.in);
         System.out.println("Your name: ");
         String name = nameIn.nextLine();
 
-        System.out.print("Hello," + name);
+        /*Asking for age*/
+        Scanner ageIn =new Scanner(System.in);
+        System.out.println("Your age: ");
+        int age = ageIn.nextInt();
+
+        /*The message*/
+        System.out.print("Hello," + name+ " you are " + age + " years old!!");
 
 
 
