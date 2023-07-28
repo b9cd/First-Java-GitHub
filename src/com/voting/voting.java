03c2e3f171f1134bd803e701db7814d201b519e1
ABDULRHMAN <abdsati6@icloud.com>
2023-07-29 02:08:28 +0300

    Printing name

--- src/com/voting/voting.java
+++ src/com/voting/voting.java
@@ -1,4 +1,16 @@
 package com.voting;
+import java.util.Scanner;
 
 public class voting {
+    public static void main(String[] args){
+
+        Scanner nameIn = new Scanner(System.in);
+        System.out.println("Your name: ");
+        String name = nameIn.nextLine();
+
+        System.out.print(name);
+
+
+
+    }
 }
